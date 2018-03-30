e025ba78571dcbc764c380d13741abbe4c68b376
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-03-30 17:03:27 -0400

    Switch to SFC for app component.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 
-export class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <nav className="pt-navbar">
-          <div className="pt-navbar-group pt-align-left">
-            <div className="pt-navbar-heading">Sotah Client</div>
-          </div>
-        </nav>
-        <p>Hello, world!</p>
-      </div>
-    );
-  }
-}
+export const App: React.SFC = () => {
+  return (
+    <div>
+      <nav className="pt-navbar">
+        <div className="pt-navbar-group pt-align-left">
+          <div className="pt-navbar-heading">Sotah Client</div>
+        </div>
+      </nav>
+      <p>Hello, world!</p>
+    </div>
+  );
+};
