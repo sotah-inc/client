37df96d25434aa310bb0c59b5aca51dd5be4958c
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-03-30 15:01:39 -0400

    Start get-regions functionality.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Features
 
-- [ ] Use Yarn (not NPM)
-- [ ] Use Microsoft's TS-based React starter kit
+- [x] Use Yarn (not NPM)
+- [x] Use Microsoft's TS-based React starter kit
 - [ ] Use [Plottable](https://github.com/palantir/plottable) or [Recharts](https://github.com/recharts/recharts)
-- [ ] Use [Blueprint](http://blueprintjs.com/) (components, not design methodolgy)
+- [x] Use [Blueprint](http://blueprintjs.com/) (components, not design methodolgy)
 - [ ] Use [Polaris](https://polaris.shopify.com/) (design methodology, not components)
 - [ ] Use [Formik](https://github.com/jaredpalmer/formik)
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import { App } from './components/App';
+import { sotah } from './reducers';
+import { StoreState } from './types';
 import registerServiceWorker from './registerServiceWorker';
 
+const preloadedState: StoreState = {};
+const store = createStore<StoreState>(sotah, preloadedState);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
