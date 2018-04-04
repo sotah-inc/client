d0ce41d352d88f6028a8ee1923d468792765456c
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-04-03 20:15:41 -0400

    Implement custom rendering of realm-toggle list, add missing filtering of realm-toggle list.

--- src/components/App/RealmToggle.tsx
+++ src/components/App/RealmToggle.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
-import { Button, MenuItem, Spinner } from '@blueprintjs/core';
-import { Select, ItemPredicate, ItemRenderer, IItemRendererProps } from '@blueprintjs/select';
+import { Button, Menu, MenuItem, Spinner } from '@blueprintjs/core';
+import {
+  Select,
+  ItemPredicate,
+  ItemListRenderer,
+  IItemListRendererProps,
+  ItemRenderer,
+  IItemRendererProps
+} from '@blueprintjs/select';
 
 import { Realms, Realm } from '../../types';
 
@@ -20,18 +27,37 @@ type Props = Readonly<StateProps & DispatchProps & OwnProps>;
 
 export class RealmToggle extends React.Component<Props> {
   itemPredicate: ItemPredicate<Realm> = (query: string, item: Realm) => {
-    return item.name.toLowerCase().indexOf(query.toLowerCase()) >= 0;
+    query = query.toLowerCase();
+    return item.name.toLowerCase().indexOf(query) >= 0
+      || item.battlegroup.toLowerCase().indexOf(query) >= 0;
   }
 
   itemRenderer: ItemRenderer<Realm> = (realm: Realm, { handleClick, modifiers, index }: IItemRendererProps) => {
+    if (!modifiers.matchesPredicate) {
+      return null;
+    }
+
     return (
       <MenuItem
         key={index}
         className={modifiers.active ? 'pt-active' : ''}
-        label={realm.name}
+        label={realm.battlegroup}
         onClick={handleClick}
         text={realm.name}
       />
+    );
+  }
+
+  itemListRenderer: ItemListRenderer<Realm> =  (params: IItemListRendererProps<Realm>) => {
+    const { items, itemsParentRef, renderItem } = params;
+    const renderedItems = items.map(renderItem).filter((renderedItem) => renderedItem !== null);
+    return (
+      <Menu ulRef={itemsParentRef}>
+        <li>
+          <h6>Select Realm</h6>
+        </li>
+        {renderedItems}
+      </Menu>
     );
   }
 
@@ -46,8 +72,11 @@ export class RealmToggle extends React.Component<Props> {
       <RealmToggleSelect
         items={items}
         itemRenderer={this.itemRenderer}
-        noResults={<MenuItem disabled={true} text="No results." />}
+        itemListRenderer={this.itemListRenderer}
+        itemPredicate={this.itemPredicate}
         onItemSelect={(realm: Realm) => onRealmChange(realm)}
+        resetOnSelect={true}
+        resetOnClose={true}
       >
         <Button text={items[0].name} rightIcon="double-caret-vertical" />
       </RealmToggleSelect>
