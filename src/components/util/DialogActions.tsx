f01e71f45210f0706a91eb35d94cc79c46b15689
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-04-08 10:13:58 -0400

    Add missing dialog-actions and dialog-footer components.

---
+++ src/components/util/DialogActions.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+import { DialogFooter } from './DialogFooter';
+
+export const DialogActions: React.SFC = (props) =>
+    <DialogFooter><div className="pt-dialog-footer-actions">{props.children}</div></DialogFooter>;
---
+++ src/components/util/DialogFooter.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+
+export const DialogFooter: React.SFC = (props) =>
+    <div className="pt-dialog-footer">{props.children}</div>;
