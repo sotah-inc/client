6f4ed0a9d6a7b146663763da6ad27bdc40249c48
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-04-08 10:20:12 -0400

    Remove register route, opting for dialog.

--- src/components/App/Content.tsx
+++ src/components/App/Content.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import RealmList from '../../containers/App/RealmList';
-import { Register } from './Register';
 import { NotFound } from '../util/NotFound';
 
 export const Content: React.SFC = () => {
@@ -10,7 +9,6 @@ export const Content: React.SFC = () => {
     <div id="content">
       <Switch>
         <Route exact={true} path="/realms" component={RealmList} />
-        <Route exact={true} path="/register" component={Register} />
         <Route component={NotFound} />
       </Switch>
     </div>
