8ef3d817afd242c2874d170c0d43c9379a7acb88
Adrian Parker <ihsw.aparker@gmail.com>
2018-06-13 21:32:45 -0400

    Add non-ideal-state element for when there are no price lists.

--- src/components/App/PriceLists.tsx
+++ src/components/App/PriceLists.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Tab, Tabs, Button } from '@blueprintjs/core';
+import { Tab, Tabs, Button, NonIdealState } from '@blueprintjs/core';
 
 import { PriceList } from '@app/types/price-lists';
 
@@ -33,6 +33,19 @@ export class PriceLists extends React.Component<Props> {
 
   render() {
     const { lists } = this.props;
+
+    if (lists.length === 0) {
+      return (
+        <div style={{marginTop: '10px'}}>
+          <NonIdealState
+            title="No price lists"
+            description="You have no price lists."
+            visual="list"
+            action={<Button className="pt-fill" icon="plus" style={{marginBottom: '10px'}}>Add List</Button>}
+          />
+        </div>
+      );
+    }
 
     return (
       <Tabs id="price-lists" selectedTabId="ayy" vertical={true}>
