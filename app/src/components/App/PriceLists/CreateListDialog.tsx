0b43c2db0308a5067ddbe2ba1015abdf18983736
Adrian Parker <ihsw.aparker@gmail.com>
2018-08-09 13:23:40 -0400

    Remove unnecessary state from create-list-dialog, add trigger to list-dialog for resetting data.

--- app/src/components/App/PriceLists/CreateListDialog.tsx
+++ app/src/components/App/PriceLists/CreateListDialog.tsx
@@ -4,8 +4,8 @@ import { Intent } from "@blueprintjs/core";
 
 import { ICreatePricelistRequest } from "@app/api/price-lists";
 import { ListDialog } from "@app/components/App/PriceLists/util/ListDialog";
-import { IErrors, IProfile, IRealm, IRegion, ItemsMap } from "@app/types/global";
-import { CreateListCompletion, CreateListStep, IPricelistEntry, MutatePricelistLevel } from "@app/types/price-lists";
+import { IErrors, IProfile, IRealm, IRegion } from "@app/types/global";
+import { MutatePricelistLevel } from "@app/types/price-lists";
 import { AppToaster } from "@app/util/toasters";
 
 export interface IStateProps {
@@ -25,24 +25,17 @@ export interface IDispatchProps {
 export type Props = Readonly<IStateProps & IDispatchProps>;
 
 type State = Readonly<{
-    createListStep: CreateListStep;
-    listName: string;
-    createListCompletion: CreateListCompletion;
-    entries: IPricelistEntry[];
-    entriesItems: ItemsMap;
+    listDialogResetTrigger: number;
 }>;
 
 export class CreateListDialog extends React.Component<Props, State> {
     public state = {
-        createListCompletion: CreateListCompletion.initial,
-        createListStep: CreateListStep.list,
-        entries: [],
-        entriesItems: {},
-        listName: "",
+        listDialogResetTrigger: 0,
     };
 
     public componentDidUpdate(prevProps: Props) {
         const { createPricelistLevel } = this.props;
+        const { listDialogResetTrigger } = this.state;
 
         if (prevProps.createPricelistLevel !== createPricelistLevel) {
             switch (createPricelistLevel) {
@@ -52,13 +45,7 @@ export class CreateListDialog extends React.Component<Props, State> {
                         intent: Intent.SUCCESS,
                         message: "Your pricelist has been created.",
                     });
-                    this.setState({
-                        createListCompletion: CreateListCompletion.initial,
-                        createListStep: CreateListStep.list,
-                        entries: [],
-                        entriesItems: {},
-                        listName: "",
-                    });
+                    this.setState({ listDialogResetTrigger: listDialogResetTrigger + 1 });
 
                     break;
                 default:
@@ -78,6 +65,7 @@ export class CreateListDialog extends React.Component<Props, State> {
             currentRegion,
             currentRealm,
         } = this.props;
+        const { listDialogResetTrigger } = this.state;
 
         return (
             <ListDialog
@@ -86,6 +74,7 @@ export class CreateListDialog extends React.Component<Props, State> {
                 title="New Price List"
                 mutationErrors={createPricelistErrors}
                 mutatePricelistLevel={createPricelistLevel}
+                resetTrigger={listDialogResetTrigger}
                 onComplete={({ name, entries }) => {
                     createPricelist(profile!.token, {
                         entries,
