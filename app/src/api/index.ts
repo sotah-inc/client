a6c572130f314ea118c506e45248c3664612ce3d
Adrian Parker <ihsw.aparker@gmail.com>
2018-08-11 16:45:03 -0400

    Update api-host to be different depending on local vs non-local.

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -1,1 +1,5 @@
-export const apiEndpoint = `//${window.location.hostname}:8080`;
+const hostname = window.location.hostname;
+export let apiHost = `//${window.location.hostname}:8080`;
+if (hostname !== "localhost") {
+    apiHost = `//api.${hostname}`;
+}
