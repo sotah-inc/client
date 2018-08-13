f4238d7e3df5c9833a0aceca4e03ac0625225764
Adrian Parker <ihsw.aparker@gmail.com>
2018-08-13 08:09:57 -0400

    Fix more func visibility levels.

--- app/src/components/App/AuctionList/QueryAuctionsFilter.tsx
+++ app/src/components/App/AuctionList/QueryAuctionsFilter.tsx
@@ -58,182 +58,6 @@ export class QueryAuctionsFilter extends React.Component<Props, State> {
 
     private debouncedTriggerQuery = debounce((filterValue: string) => this.triggerQuery(filterValue), 0.25 * 1000);
 
-    public tagRenderer(result: IQueryAuctionResult) {
-        const { item, owner } = result;
-        if (item.name !== "") {
-            return item.name;
-        } else if (owner.name !== "") {
-            return owner.name;
-        }
-
-        return "n/a";
-    }
-
-    public itemPredicate: ItemPredicate<IQueryAuctionResult> = (_query: string, result: IQueryAuctionResult) => {
-        return result.rank > -1;
-    };
-
-    public renderItemAsItemRendererText(item: Item) {
-        const itemText = getItemTextValue(item);
-        const itemIconUrl = getItemIconUrl(item);
-
-        if (itemIconUrl === null) {
-            return itemText;
-        }
-
-        return (
-            <>
-                <img src={itemIconUrl} className="item-icon" /> {itemText}
-            </>
-        );
-    }
-
-    public renderItemRendererTextContent(result: IQueryAuctionResult) {
-        const { item, owner } = result;
-
-        if (item.id > 0) {
-            return this.renderItemAsItemRendererText(result.item);
-        } else if (owner.name !== "") {
-            return owner.name;
-        }
-
-        return "n/a";
-    }
-
-    public renderItemRendererText(result: IQueryAuctionResult) {
-        return <span className="qaf-menu-item">{this.renderItemRendererTextContent(result)}</span>;
-    }
-
-    public itemRenderer: ItemRenderer<IQueryAuctionResult> = (
-        result: IQueryAuctionResult,
-        { handleClick, modifiers, index }: IItemRendererProps,
-    ) => {
-        if (!modifiers.matchesPredicate) {
-            return null;
-        }
-
-        const { item, owner } = result;
-        let className = modifiers.active ? Classes.ACTIVE : "";
-
-        let label = "n/a";
-        if (item.name !== "") {
-            label = `#${item.id}`;
-            className = `${className} ${qualityToColorClass(result.item.quality)}`;
-        } else if (owner.name !== "") {
-            label = "Owner";
-        }
-
-        return (
-            <MenuItem
-                key={index}
-                icon={this.isResultSelected(result) ? "tick" : "blank"}
-                className={className}
-                onClick={handleClick}
-                text={this.renderItemRendererText(result)}
-                label={label}
-            />
-        );
-    };
-
-    public itemListRenderer: ItemListRenderer<IQueryAuctionResult> = (
-        params: IItemListRendererProps<IQueryAuctionResult>,
-    ) => {
-        const { items, itemsParentRef, renderItem } = params;
-        const renderedItems = items.map(renderItem).filter(renderedItem => renderedItem !== null);
-        if (renderedItems.length === 0) {
-            return (
-                <Menu ulRef={itemsParentRef}>
-                    <li>
-                        <H6>Queried Results</H6>
-                    </li>
-                    <li>
-                        <em>No results found.</em>
-                    </li>
-                </Menu>
-            );
-        }
-
-        return (
-            <Menu ulRef={itemsParentRef} className="qaf-menu">
-                <li>
-                    <H6>Queried Results</H6>
-                </li>
-                {renderedItems}
-            </Menu>
-        );
-    };
-
-    public onItemSelect(result: IQueryAuctionResult) {
-        const { onAuctionsQueryDeselect, onAuctionsQuerySelect } = this.props;
-
-        if (this.isResultSelected(result)) {
-            onAuctionsQueryDeselect(this.getSelectedResultIndex(result));
-
-            return;
-        }
-
-        onAuctionsQuerySelect(result);
-    }
-
-    public triggerQuery(filterValue: string) {
-        const { refreshAuctionsQuery, currentRealm, currentRegion } = this.props;
-
-        refreshAuctionsQuery({
-            query: filterValue,
-            realmSlug: currentRealm!.slug,
-            regionName: currentRegion!.name,
-        });
-    }
-
-    public isResultSelected(result: IQueryAuctionResult) {
-        return this.getSelectedResultIndex(result) > -1;
-    }
-
-    public getSelectedResultIndex(result: IQueryAuctionResult): number {
-        const selectedItems = this.props.selectedItems;
-        return getSelectedResultIndex(result, selectedItems);
-    }
-
-    public inputValueRenderer(result: IQueryAuctionResult): string {
-        const { item, owner } = result;
-
-        if (item.id > 0) {
-            return getItemTextValue(item);
-        } else if (owner.name !== "") {
-            return owner.name;
-        }
-
-        return "n/a";
-    }
-
-    public renderSelectedItem(index: number, result: IQueryAuctionResult) {
-        const { onAuctionsQueryDeselect } = this.props;
-
-        return (
-            <Tag key={index} onRemove={() => onAuctionsQueryDeselect(index)} style={{ marginRight: "5px" }}>
-                {this.inputValueRenderer(result)}
-            </Tag>
-        );
-    }
-
-    public renderSelectedItems() {
-        const { selectedItems } = this.props;
-        if (selectedItems.length === 0) {
-            return;
-        }
-
-        return (
-            <Callout>
-                <H4 className={Classes.HEADING}>Filters</H4>
-                <div className={Classes.TAG_INPUT}>
-                    <div className={Classes.TAG_INPUT_VALUES}>
-                        {selectedItems.map((v, i) => this.renderSelectedItem(i, v))}
-                    </div>
-                </div>
-            </Callout>
-        );
-    }
-
     public render() {
         const { queryAuctionsLevel, items } = this.props;
 
@@ -271,5 +95,170 @@ export class QueryAuctionsFilter extends React.Component<Props, State> {
             default:
                 return <Spinner className={Classes.SMALL} intent={Intent.PRIMARY} />;
         }
+    }
+
+    private itemPredicate: ItemPredicate<IQueryAuctionResult> = (_query: string, result: IQueryAuctionResult) => {
+        return result.rank > -1;
+    };
+
+    private renderItemAsItemRendererText(item: Item) {
+        const itemText = getItemTextValue(item);
+        const itemIconUrl = getItemIconUrl(item);
+
+        if (itemIconUrl === null) {
+            return itemText;
+        }
+
+        return (
+            <>
+                <img src={itemIconUrl} className="item-icon" /> {itemText}
+            </>
+        );
+    }
+
+    private renderItemRendererTextContent(result: IQueryAuctionResult) {
+        const { item, owner } = result;
+
+        if (item.id > 0) {
+            return this.renderItemAsItemRendererText(result.item);
+        } else if (owner.name !== "") {
+            return owner.name;
+        }
+
+        return "n/a";
+    }
+
+    private renderItemRendererText(result: IQueryAuctionResult) {
+        return <span className="qaf-menu-item">{this.renderItemRendererTextContent(result)}</span>;
+    }
+
+    private itemRenderer: ItemRenderer<IQueryAuctionResult> = (
+        result: IQueryAuctionResult,
+        { handleClick, modifiers, index }: IItemRendererProps,
+    ) => {
+        if (!modifiers.matchesPredicate) {
+            return null;
+        }
+
+        const { item, owner } = result;
+        let className = modifiers.active ? Classes.ACTIVE : "";
+
+        let label = "n/a";
+        if (item.name !== "") {
+            label = `#${item.id}`;
+            className = `${className} ${qualityToColorClass(result.item.quality)}`;
+        } else if (owner.name !== "") {
+            label = "Owner";
+        }
+
+        return (
+            <MenuItem
+                key={index}
+                icon={this.isResultSelected(result) ? "tick" : "blank"}
+                className={className}
+                onClick={handleClick}
+                text={this.renderItemRendererText(result)}
+                label={label}
+            />
+        );
+    };
+
+    private itemListRenderer: ItemListRenderer<IQueryAuctionResult> = (
+        params: IItemListRendererProps<IQueryAuctionResult>,
+    ) => {
+        const { items, itemsParentRef, renderItem } = params;
+        const renderedItems = items.map(renderItem).filter(renderedItem => renderedItem !== null);
+        if (renderedItems.length === 0) {
+            return (
+                <Menu ulRef={itemsParentRef}>
+                    <li>
+                        <H6>Queried Results</H6>
+                    </li>
+                    <li>
+                        <em>No results found.</em>
+                    </li>
+                </Menu>
+            );
+        }
+
+        return (
+            <Menu ulRef={itemsParentRef} className="qaf-menu">
+                <li>
+                    <H6>Queried Results</H6>
+                </li>
+                {renderedItems}
+            </Menu>
+        );
+    };
+
+    private onItemSelect(result: IQueryAuctionResult) {
+        const { onAuctionsQueryDeselect, onAuctionsQuerySelect } = this.props;
+
+        if (this.isResultSelected(result)) {
+            onAuctionsQueryDeselect(this.getSelectedResultIndex(result));
+
+            return;
+        }
+
+        onAuctionsQuerySelect(result);
+    }
+
+    private triggerQuery(filterValue: string) {
+        const { refreshAuctionsQuery, currentRealm, currentRegion } = this.props;
+
+        refreshAuctionsQuery({
+            query: filterValue,
+            realmSlug: currentRealm!.slug,
+            regionName: currentRegion!.name,
+        });
+    }
+
+    private isResultSelected(result: IQueryAuctionResult) {
+        return this.getSelectedResultIndex(result) > -1;
+    }
+
+    private getSelectedResultIndex(result: IQueryAuctionResult): number {
+        const selectedItems = this.props.selectedItems;
+        return getSelectedResultIndex(result, selectedItems);
+    }
+
+    private inputValueRenderer(result: IQueryAuctionResult): string {
+        const { item, owner } = result;
+
+        if (item.id > 0) {
+            return getItemTextValue(item);
+        } else if (owner.name !== "") {
+            return owner.name;
+        }
+
+        return "n/a";
+    }
+
+    private renderSelectedItem(index: number, result: IQueryAuctionResult) {
+        const { onAuctionsQueryDeselect } = this.props;
+
+        return (
+            <Tag key={index} onRemove={() => onAuctionsQueryDeselect(index)} style={{ marginRight: "5px" }}>
+                {this.inputValueRenderer(result)}
+            </Tag>
+        );
+    }
+
+    private renderSelectedItems() {
+        const { selectedItems } = this.props;
+        if (selectedItems.length === 0) {
+            return;
+        }
+
+        return (
+            <Callout>
+                <H4 className={Classes.HEADING}>Filters</H4>
+                <div className={Classes.TAG_INPUT}>
+                    <div className={Classes.TAG_INPUT_VALUES}>
+                        {selectedItems.map((v, i) => this.renderSelectedItem(i, v))}
+                    </div>
+                </div>
+            </Callout>
+        );
     }
 }
