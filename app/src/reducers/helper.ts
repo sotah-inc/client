2ffea3d48f64586dbec97d46a8d1872403f4e64f
Adrian Parker <ihsw.aparker@gmail.com>
2018-08-19 20:43:06 -0400

    Remove dead code.

--- app/src/reducers/helper.ts
+++ app/src/reducers/helper.ts
@@ -1,10 +1,5 @@
-import { ExpansionName, IExpansion, IProfessionPricelist } from "@app/types/global";
-import {
-    DeletePricelistLevel,
-    IExpansionProfessionPricelistMap,
-    IPricelist,
-    IPriceListsState,
-} from "@app/types/price-lists";
+import { IExpansion, IProfessionPricelist } from "@app/types/global";
+import { IExpansionProfessionPricelistMap, IPricelist, IPriceListsState } from "@app/types/price-lists";
 
 export const getPricelistIndex = (pricelists: IPricelist[], id: number): number => {
     for (let i = 0; i < pricelists.length; i++) {
@@ -64,59 +59,4 @@ export const getProfessionPricelist = (state: IPriceListsState, pricelist: IPric
     }
 
     return null;
-};
-
-interface IDeleteSearchResult {
-    index: number;
-    expansion: ExpansionName;
-}
-
-export const handleDeleteProfessionPricelistSuccess = (
-    state: IPriceListsState,
-    pricelistId: number,
-): IPriceListsState => {
-    const stateProfessionPricelists = { ...state.professionPricelists };
-    const searchResult: IDeleteSearchResult | null = Object.keys(stateProfessionPricelists).reduce(
-        (result: IDeleteSearchResult | null, expansionName) => {
-            const professionPricelists = stateProfessionPricelists[expansionName];
-            for (let i = 0; i < professionPricelists.length; i++) {
-                const professionPricelist = professionPricelists[i];
-                if (professionPricelist.pricelist_id === pricelistId) {
-                    return { index: Number(i), expansion: expansionName };
-                }
-            }
-
-            return result;
-        },
-        null,
-    );
-    if (searchResult === null) {
-        return { ...state, deletePricelistLevel: DeletePricelistLevel.failure };
-    }
-
-    const { expansion, index: deletedIndex } = searchResult;
-    let onDeletePricelist = [
-        ...stateProfessionPricelists[expansion].slice(0, deletedIndex),
-        ...stateProfessionPricelists[expansion].slice(deletedIndex + 1),
-    ];
-    if (deletedIndex === 0) {
-        onDeletePricelist = stateProfessionPricelists[expansion].slice(1);
-    }
-    stateProfessionPricelists[expansion] = onDeletePricelist;
-
-    let onDeleteSelectedList: IPricelist | null = null;
-    if (stateProfessionPricelists[expansion].length > 0) {
-        const isLastDeleted = deletedIndex === onDeletePricelist.length;
-        onDeleteSelectedList = isLastDeleted
-            ? onDeletePricelist[deletedIndex - 1].pricelist!
-            : onDeletePricelist[deletedIndex].pricelist!;
-    }
-
-    return {
-        ...state,
-        deletePricelistLevel: DeletePricelistLevel.success,
-        isDeleteListDialogOpen: false,
-        professionPricelists: stateProfessionPricelists,
-        selectedList: onDeleteSelectedList,
-    };
 };
