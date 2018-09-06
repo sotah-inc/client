763622fa68cfe6a6a326630585dca7b19de2d5db
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-06 09:04:20 -0400

    Update history graph to have tabs for swapping between graph data kinds, add volume graph.

--- app/src/components/App/PriceLists/PricelistTree/PricelistPanel/PricelistTable/PricelistHistoryGraph.tsx
+++ app/src/components/App/PriceLists/PricelistTree/PricelistPanel/PricelistTable/PricelistHistoryGraph.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { H4, Intent, Spinner } from "@blueprintjs/core";
+import { H4, Intent, Spinner, Tab, Tabs } from "@blueprintjs/core";
 import * as moment from "moment";
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
@@ -33,7 +33,20 @@ interface ILineItem {
 
 type Props = Readonly<IStateProps & IDispatchProps & IOwnProps>;
 
-export class PricelistHistoryGraph extends React.Component<Props> {
+type State = Readonly<{
+    currentTabKind: string;
+}>;
+
+enum TabKind {
+    prices = "prices",
+    volume = "volume",
+}
+
+export class PricelistHistoryGraph extends React.Component<Props, State> {
+    public state: State = {
+        currentTabKind: TabKind.prices,
+    };
+
     public componentDidMount() {
         const { region, realm, reloadPricelistHistory, list } = this.props;
 
@@ -70,12 +83,74 @@ export class PricelistHistoryGraph extends React.Component<Props> {
     }
 
     public render() {
+        const { currentTabKind } = this.state;
+
         return (
             <>
-                <H4>Price History</H4>
-                {this.renderContent()}
+                <H4>History</H4>
+                <div style={{ marginBottom: "10px" }}>
+                    <Tabs
+                        id="history-tabs"
+                        selectedTabId={currentTabKind}
+                        onChange={tabKind => this.setState({ currentTabKind: tabKind.toString() })}
+                    >
+                        <Tab id={TabKind.prices} title="Prices" />
+                        <Tab id={TabKind.volume} title="Volume" />
+                    </Tabs>
+                </div>
+                <div style={{ marginBottom: "10px" }}>{this.renderContent()}</div>
             </>
         );
+    }
+
+    private renderYAxis() {
+        const { currentTabKind } = this.state;
+
+        switch (currentTabKind) {
+            case TabKind.volume:
+                return (
+                    <YAxis
+                        tickFormatter={v => Number(v).toLocaleString()}
+                        domain={[
+                            dataMin => {
+                                const result = Math.pow(10, Math.floor(Math.log10(dataMin)));
+                                if (result === 0) {
+                                    return 10;
+                                }
+
+                                return result;
+                            },
+                            dataMax => Math.pow(10, Math.ceil(Math.log10(dataMax))),
+                        ]}
+                        tick={{ fill: "#fff" }}
+                        scale="log"
+                        allowDataOverflow={true}
+                        mirror={true}
+                    />
+                );
+            case TabKind.prices:
+            default:
+                return (
+                    <YAxis
+                        tickFormatter={v => currencyToText(v * 10 * 10)}
+                        domain={[
+                            dataMin => {
+                                const result = Math.pow(10, Math.floor(Math.log10(dataMin)));
+                                if (result === 0) {
+                                    return 10;
+                                }
+
+                                return result;
+                            },
+                            dataMax => Math.pow(10, Math.ceil(Math.log10(dataMax))),
+                        ]}
+                        tick={{ fill: "#fff" }}
+                        scale="log"
+                        allowDataOverflow={true}
+                        mirror={true}
+                    />
+                );
+        }
     }
 
     private renderContent() {
@@ -102,7 +177,11 @@ export class PricelistHistoryGraph extends React.Component<Props> {
                     const unixTimestamp = Number(unixTimestampKey);
                     const prices = itemPricelistHistory[unixTimestamp];
 
-                    previousValue.push({ name: unixTimestamp, [`${itemId}_buyout`]: prices.buyout / 10 / 10 });
+                    previousValue.push({
+                        name: unixTimestamp,
+                        [`${itemId}_buyout`]: prices.buyout / 10 / 10,
+                        [`${itemId}_volume`]: prices.volume,
+                    });
 
                     return previousValue;
                 }, dataPreviousValue);
@@ -124,7 +203,7 @@ export class PricelistHistoryGraph extends React.Component<Props> {
             .subtract(nowDate.seconds(), "seconds")
             .add(12, "hours");
 
-        const ticks = Array.from(Array(9)).map((_, i) => {
+        const xAxisTicks = Array.from(Array(9)).map((_, i) => {
             return roundedTwoWeeksAgoDate.unix() + i * 60 * 60 * 24 * 2;
         });
 
@@ -137,27 +216,10 @@ export class PricelistHistoryGraph extends React.Component<Props> {
                         tickFormatter={unixTimestampToText}
                         domain={[roundedTwoWeeksAgoDate.unix(), roundedNowDate.unix()]}
                         type="number"
-                        ticks={ticks}
+                        ticks={xAxisTicks}
                         tick={{ fill: "#fff" }}
                     />
-                    <YAxis
-                        tickFormatter={v => currencyToText(v * 10 * 10)}
-                        domain={[
-                            dataMin => {
-                                const result = Math.pow(10, Math.floor(Math.log10(dataMin)));
-                                if (result === 0) {
-                                    return 10;
-                                }
-
-                                return result;
-                            },
-                            dataMax => Math.pow(10, Math.ceil(Math.log10(dataMax)) + 1),
-                        ]}
-                        tick={{ fill: "#fff" }}
-                        scale="log"
-                        allowDataOverflow={true}
-                        mirror={true}
-                    />
+                    {this.renderYAxis()}
                     <Legend />
                     {this.renderLines(pricelistHistoryMap)}
                 </LineChart>
@@ -175,6 +237,18 @@ export class PricelistHistoryGraph extends React.Component<Props> {
         return null;
     }
 
+    private getDataKey(itemId: ItemId) {
+        const { currentTabKind } = this.state;
+
+        switch (currentTabKind) {
+            case TabKind.volume:
+                return `${itemId}_volume`;
+            case TabKind.prices:
+            default:
+                return `${itemId}_buyout`;
+        }
+    }
+
     private renderLine(index: number, itemId: ItemId) {
         let name: string = itemId.toString();
         const item = this.getItem(itemId);
@@ -186,7 +260,7 @@ export class PricelistHistoryGraph extends React.Component<Props> {
             <Line
                 key={index}
                 name={name}
-                dataKey={`${itemId}_buyout`}
+                dataKey={this.getDataKey(itemId)}
                 stroke={getColor(index)}
                 dot={false}
                 animationEasing={"ease-in-out"}
