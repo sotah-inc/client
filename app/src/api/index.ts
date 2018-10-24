94ad1e056531aeb12202187e523a589d75431e77
Adrian Parker <ihsw.aparker@gmail.com>
2018-10-24 10:33:16 -0400

    Create gather func for making typed http requests.

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -3,3 +3,40 @@ export let apiEndpoint = `//${window.location.hostname}:8080`;
 if (hostname !== "localhost") {
     apiEndpoint = `//api.${hostname}`;
 }
+
+export interface IGatherOptions<T> {
+    headers?: Headers;
+    body?: T;
+    method?: string;
+    url: string;
+}
+
+export interface IGatherResult<T> {
+    response: Response;
+    body: T;
+    status: number;
+}
+
+export const gather = async <T, A>(opts: IGatherOptions<T>): Promise<IGatherResult<A>> => {
+    const body = typeof opts.body === "undefined" ? null : JSON.stringify(opts.body);
+    const method = typeof opts.method === "undefined" ? "GET" : opts.method;
+    const headers: Headers = (() => {
+        if (typeof opts.headers === "undefined") {
+            return new Headers({ "content-type": "application/json" });
+        }
+
+        return opts.headers;
+    })();
+
+    const response = await fetch(opts.url, {
+        body,
+        headers,
+        method,
+    });
+
+    return {
+        body: (await response.json()) as A,
+        response,
+        status: response.status,
+    };
+};
