b56fc248b0526d843c6fb725f1e1f2a319f0505c
Adrian Parker <ihsw.aparker@gmail.com>
2018-11-08 21:22:00 -0500

    Add button-props prop to link-button component, start dividing top-nav into main top-nav and sub-nav.

--- app/src/components/App/Topbar.tsx
+++ app/src/components/App/Topbar.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
 
-import { Alignment, ButtonGroup, Classes, Navbar, NavbarDivider, NavbarGroup, NavbarHeading } from "@blueprintjs/core";
+import {
+    Alignment,
+    Button,
+    ButtonGroup,
+    Classes,
+    Navbar,
+    NavbarDivider,
+    NavbarGroup,
+    NavbarHeading,
+} from "@blueprintjs/core";
 import { RouteComponentProps } from "react-router-dom";
 
 import { IUserJson } from "@app/api-types/entities";
@@ -28,7 +37,7 @@ export class Topbar extends React.Component<Props> {
             );
         }
 
-        return <LinkButtonRouteContainer icon="user" text="Profile" destination="/profile" />;
+        return <LinkButtonRouteContainer destination="/profile" buttonProps={{ icon: "user", text: "Profile" }} />;
     }
 
     public render() {
@@ -39,13 +48,44 @@ export class Topbar extends React.Component<Props> {
                         <NavbarGroup align={Alignment.LEFT}>
                             <NavbarHeading>Sotah Client</NavbarHeading>
                             <NavbarDivider />
+                            <LinkButtonRouteContainer
+                                destination="/content"
+                                buttonProps={{ icon: "manually-entered-data", text: "Content" }}
+                            />
+                            <NavbarDivider />
+                            <Button icon="chart">Data</Button>
+                            <NavbarDivider />
                             <ButtonGroup>
-                                <LinkButtonRouteContainer icon="home" text="Home" destination="/" />
-                                <LinkButtonRouteContainer icon="list" text="Auctions" destination="/auctions" />
-                                <LinkButtonRouteContainer icon="list" text="Price Lists" destination="/price-lists" />
+                                <LinkButtonRouteContainer
+                                    destination="/"
+                                    buttonProps={{ icon: "home", text: "Home" }}
+                                />
+                                <LinkButtonRouteContainer
+                                    destination="/auctions"
+                                    buttonProps={{ icon: "list", text: "Auctions" }}
+                                />
+                                <LinkButtonRouteContainer
+                                    destination="/price-lists"
+                                    buttonProps={{ icon: "list", text: "Price Lists" }}
+                                />
                             </ButtonGroup>
                             <NavbarDivider />
                             {this.renderUserInfo()}
+                        </NavbarGroup>
+                    </div>
+                </Navbar>
+                <Navbar className={Classes.DARK}>
+                    <div id="topbar">
+                        <NavbarGroup align={Alignment.LEFT}>
+                            <LinkButtonRouteContainer
+                                destination="/content/news"
+                                buttonProps={{ icon: "globe-network", text: "News", minimal: true }}
+                            />
+                            <NavbarDivider />
+                            <LinkButtonRouteContainer
+                                destination="/content/feed"
+                                buttonProps={{ icon: "feed", text: "Feed", minimal: true }}
+                            />
                         </NavbarGroup>
                     </div>
                 </Navbar>
--- app/src/components/util/LinkButton.tsx
+++ app/src/components/util/LinkButton.tsx
@@ -1,23 +1,17 @@
 import * as React from "react";
 
-import { Button, IconName } from "@blueprintjs/core";
+import { Button, IButtonProps } from "@blueprintjs/core";
 import { RouteComponentProps } from "react-router-dom";
 
 export interface IProps extends RouteComponentProps<{}> {
-    icon: IconName;
-    text: string;
     destination: string;
+    buttonProps: IButtonProps;
 }
 
 export const LinkButton: React.SFC<IProps> = (props: IProps) => {
-    const { destination, location, icon, text, history } = props;
+    const { destination, location, history, buttonProps } = props;
 
     return (
-        <Button
-            icon={icon}
-            text={text}
-            active={location.pathname === destination}
-            onClick={() => history.push(destination)}
-        />
+        <Button active={location.pathname === destination} onClick={() => history.push(destination)} {...buttonProps} />
     );
 };
