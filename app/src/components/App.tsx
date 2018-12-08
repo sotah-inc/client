7ffe3cda4b49ab339f30bcd4f561ac1c76d34392
Adrian Parker <ihsw.aparker@gmail.com>
2018-12-08 14:43:46 -0500

    Clean up logic flow, ensuring app child components are rendered only after boot-level is success.

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { Classes, Intent } from "@blueprintjs/core";
+import { Classes, Intent, NonIdealState, Spinner } from "@blueprintjs/core";
 import { RouteComponentProps } from "react-router-dom";
 
 import { IPreferenceJson } from "@app/api-types/entities";
@@ -73,7 +73,7 @@ export class App extends React.Component<Props> {
                     });
                 }
 
-                this.handleLoaded(prevProps);
+                this.handleConnected(prevProps);
 
                 return;
             default:
@@ -100,13 +100,113 @@ export class App extends React.Component<Props> {
     private renderConnected() {
         return (
             <div id="app" className={`${Classes.DARK} dark-app`}>
-                <TopbarRouteContainer />
-                <ViewportRouteContainer />
+                {this.renderConnectedContent()}
             </div>
         );
     }
 
-    private handleLoaded(prevProps: Props) {
+    private renderConnectedContent() {
+        const { authLevel } = this.props;
+
+        switch (authLevel) {
+            case AuthLevel.authenticated:
+                return this.renderAuth();
+            case AuthLevel.unauthenticated:
+                return this.renderUnauth();
+            case AuthLevel.initial:
+            default:
+                return (
+                    <NonIdealState
+                        title="Loading"
+                        icon={<Spinner className={Classes.LARGE} intent={Intent.NONE} value={0} />}
+                    />
+                );
+        }
+    }
+
+    private renderAuth() {
+        const { fetchBootLevel } = this.props;
+
+        switch (fetchBootLevel) {
+            case FetchLevel.success:
+                return this.renderBootAuth();
+            case FetchLevel.fetching:
+                return (
+                    <NonIdealState
+                        title="Loading"
+                        icon={<Spinner className={Classes.LARGE} intent={Intent.PRIMARY} />}
+                    />
+                );
+            case FetchLevel.failure:
+                return (
+                    <NonIdealState
+                        title="Failed to load"
+                        icon={<Spinner className={Classes.LARGE} intent={Intent.DANGER} value={1} />}
+                    />
+                );
+            case FetchLevel.initial:
+            default:
+                return (
+                    <NonIdealState
+                        title="Loading"
+                        icon={<Spinner className={Classes.LARGE} intent={Intent.NONE} value={0} />}
+                    />
+                );
+        }
+    }
+
+    private renderBootAuth() {
+        const { fetchUserPreferencesLevel } = this.props;
+
+        switch (fetchUserPreferencesLevel) {
+            case FetchLevel.fetching:
+            case FetchLevel.refetching:
+            case FetchLevel.prompted:
+                return (
+                    <NonIdealState
+                        title="Loading"
+                        icon={<Spinner className={Classes.LARGE} intent={Intent.PRIMARY} />}
+                    />
+                );
+            case FetchLevel.success:
+                return this.renderBootAuthWithPreferences();
+            case FetchLevel.failure:
+                return (
+                    <NonIdealState
+                        title="Failed to load user preferences."
+                        icon={<Spinner className={Classes.LARGE} intent={Intent.DANGER} value={1} />}
+                    />
+                );
+            case FetchLevel.initial:
+            default:
+                return (
+                    <NonIdealState
+                        title="Loading"
+                        icon={<Spinner className={Classes.LARGE} intent={Intent.NONE} value={0} />}
+                    />
+                );
+        }
+    }
+
+    private renderBootAuthWithPreferences() {
+        return (
+            <>
+                <TopbarRouteContainer />
+                <ViewportRouteContainer />
+            </>
+        );
+    }
+
+    private renderUnauth() {
+        return (
+            <>
+                <TopbarRouteContainer />
+                <ViewportRouteContainer />
+            </>
+        );
+    }
+
+    private handleConnected(prevProps: Props) {
         const { authLevel, preloadedToken, changeAuthLevel, reloadUser } = this.props;
 
         switch (authLevel) {
