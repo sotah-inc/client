0f112d10c730f7c237e91ecc67f608f7afbd6f93
Adrian Parker <ihsw.aparker@gmail.com>
2018-12-24 14:11:19 -0500

    Fill in post-form.

--- app/src/components/App/Content/PostForm.tsx
+++ app/src/components/App/Content/PostForm.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 
+import { Button, Callout, ControlGroup, FormGroup, H2, H4, InputGroup, Intent } from "@blueprintjs/core";
 import { FormikProps } from "formik";
+import * as getSlug from "speakingurl";
 
 export interface IOwnProps {
     onComplete: (values: IFormValues) => void;
@@ -24,6 +26,119 @@ export class PostForm extends React.Component<Props, State> {
     };
 
     public render() {
-        return <p>Hello, world!</p>;
+        return (
+            <>
+                <H2>News Creator</H2>
+                <div className="pure-g">
+                    <div className="pure-u-2-5">{this.renderForm()}</div>
+                    <div className="pure-u-3-5">{this.renderPreview()}</div>
+                </div>
+            </>
+        );
+    }
+
+    // Gnoll Report 1: WoW Recession Edition
+
+    private renderPreview() {
+        return <div style={{ marginLeft: "5px" }}>{this.renderPreviewContent()}</div>;
+    }
+
+    private renderPreviewContent() {
+        const {
+            values: { title, slug },
+        } = this.props;
+
+        if (typeof title === "undefined" || title.length === 0) {
+            return (
+                <>
+                    <Callout
+                        title="Preview"
+                        icon="manually-entered-data"
+                        intent={Intent.PRIMARY}
+                        style={{ marginBottom: "10px" }}
+                    >
+                        This is a preview of the news posting.
+                    </Callout>
+                    <p>
+                        <em>Silence is golden.</em>
+                    </p>
+                </>
+            );
+        }
+
+        return (
+            <>
+                <Callout
+                    title="Preview"
+                    icon="manually-entered-data"
+                    intent={Intent.PRIMARY}
+                    style={{ marginBottom: "10px" }}
+                >
+                    This is a preview of the news posting.
+                </Callout>
+                <H4>{title}</H4>
+                <p>Slug: {`/content/news/${slug}`}</p>
+            </>
+        );
+    }
+
+    private renderForm() {
+        const { values, setFieldValue, handleSubmit, errors, touched } = this.props;
+        const { manualSlug } = this.state;
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <Callout title="Input" icon="edit" style={{ marginBottom: "10px" }}>
+                    Please enter your news post here.
+                </Callout>
+                <FormGroup
+                    helperText={errors.title}
+                    label="Title"
+                    labelFor="title"
+                    labelInfo={true}
+                    intent={errors.title && !!touched.title ? Intent.DANGER : Intent.NONE}
+                >
+                    <InputGroup
+                        intent={errors.title && !!touched.title ? Intent.DANGER : Intent.NONE}
+                        type="text"
+                        value={values.title}
+                        autoFocus={true}
+                        onChange={e => {
+                            setFieldValue("title", e.target.value);
+
+                            if (manualSlug) {
+                                return;
+                            }
+
+                            setFieldValue("slug", getSlug(e.target.value));
+                        }}
+                    />
+                </FormGroup>
+                <FormGroup
+                    helperText={errors.slug ? errors.slug : "For URLs"}
+                    label="Slug"
+                    labelFor="slug"
+                    labelInfo={true}
+                    intent={errors.slug && !!touched.slug ? Intent.DANGER : Intent.NONE}
+                >
+                    <ControlGroup fill={true}>
+                        <InputGroup
+                            intent={errors.slug && !!touched.slug ? Intent.DANGER : Intent.NONE}
+                            type="text"
+                            value={values.slug}
+                            onChange={e => setFieldValue("slug", e.target.value)}
+                            disabled={!manualSlug}
+                        />
+                        <Button
+                            active={manualSlug}
+                            icon="edit"
+                            onClick={() => this.setState({ manualSlug: !manualSlug })}
+                        >
+                            Edit
+                        </Button>
+                    </ControlGroup>
+                </FormGroup>
+            </form>
+        );
     }
 }
