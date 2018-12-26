2f83a8fca3e0d22459df5f1aae52719997df63cc
Adrian Parker <ihsw.aparker@gmail.com>
2018-12-26 11:30:41 -0500

    Fill in handlers for post creation.

--- app/src/reducers/handlers/posts.ts
+++ app/src/reducers/handlers/posts.ts
@@ -1,9 +1,31 @@
-import { PostsActions } from "@app/actions/posts";
+import { PostsActions, ReceiveCreatePost } from "@app/actions/posts";
 import { IPostsState } from "@app/types/posts";
+
+import { FetchLevel } from "@app/types/main";
 
 import { IKindHandlers, Runner } from "./index";
 
-const handlers: IKindHandlers<IPostsState, PostsActions> = {};
+const handlers: IKindHandlers<IPostsState, PostsActions> = {
+    post: {
+        create: {
+            receive: (state: IPostsState, action: ReturnType<typeof ReceiveCreatePost>) => {
+                if (action.payload === null) {
+                    return { ...state, createPostLevel: FetchLevel.failure };
+                }
+
+                return {
+                    ...state,
+                    createPostLevel: FetchLevel.success,
+                    currentPost: action.payload.post!,
+                    posts: [...state.posts, action.payload.post!],
+                };
+            },
+            request: (state: IPostsState) => {
+                return { ...state, createPostLevel: FetchLevel.fetching };
+            },
+        },
+    },
+};
 
 export const run: Runner<IPostsState, PostsActions> = (state: IPostsState, action: PostsActions): IPostsState => {
     const [kind, verb, task] = action.type
