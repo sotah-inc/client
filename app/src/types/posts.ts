46e58d454264fa1f16a67bb12e1ea893eedcc3b7
Adrian Parker <ihsw.aparker@gmail.com>
2018-12-26 11:29:55 -0500

    Add missing state.

--- app/src/types/posts.ts
+++ app/src/types/posts.ts
@@ -4,9 +4,11 @@ import { FetchLevel } from "./main";
 export interface IPostsState {
     posts: IPostJson[];
     createPostLevel: FetchLevel;
+    currentPost: IPostJson | null;
 }
 
 export const defaultPostsState: IPostsState = {
     createPostLevel: FetchLevel.initial,
+    currentPost: null,
     posts: [],
 };
