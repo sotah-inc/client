ecff96249bdad4e5b20a0f6a016f17ee2b9ef7c1
Adrian Parker <ihsw.aparker@gmail.com>
2018-12-27 09:32:56 -0500

    Fill in skeleton content loader.

--- app/src/components/App/Content/PostList.tsx
+++ app/src/components/App/Content/PostList.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import { Button, Card, Classes, H2, H5 } from "@blueprintjs/core";
+
 import { IPostJson } from "@app/api-types/entities";
 
 export interface IStateProps {
@@ -10,6 +12,40 @@ export type Props = Readonly<IStateProps>;
 
 export class PostList extends React.Component<Props> {
     public render() {
-        return <p>Hello, world!</p>;
+        return (
+            <>
+                <H2>Latest News</H2>
+                {this.renderSkeleton()}
+            </>
+        );
+    }
+
+    private renderSkeleton() {
+        const numbers: number[] = Array.apply(null, Array(3)).map((_, i: number) => i);
+
+        return numbers.map(() => this.renderSkeletonItem());
+    }
+
+    private renderSkeletonItem() {
+        return (
+            <Card style={{ marginTop: "10px" }}>
+                <H5 className={Classes.SKELETON}>
+                    <a href="#">Lorem ipsum</a>
+                </H5>
+                <p className={Classes.SKELETON}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mollis vitae nunc in
+                    tincidunt. Cras dapibus posuere ex, eget laoreet ligula ornare nec. Class aptent taciti sociosqu ad
+                    litora torquent per conubia nostra, per inceptos himenaeos. Phasellus nec justo magna. Aenean
+                    eleifend sem urna, ut dignissim lectus euismod quis. Proin pretium dignissim lacus, eu rhoncus lacus
+                    dignissim quis. Praesent nec diam nisl. Donec sit amet metus ut ligula tempus pulvinar. Nulla
+                    sodales, eros vel consequat aliquet, quam risus tempus nulla, rutrum cursus tellus ante eget diam.
+                    In iaculis laoreet nisi, sed tincidunt nunc facilisis nec. Suspendisse id tellus nec nibh vulputate
+                    pharetra. Maecenas auctor fringilla ex in ultrices. Cras leo tellus, convallis sed iaculis a,
+                    convallis eu nulla. Aenean id nibh odio. Ut convallis erat a diam lacinia volutpat. Mauris luctus
+                    tincidunt tortor eu volutpat.
+                </p>
+                <Button className={Classes.SKELETON} text="Read More" />
+            </Card>
+        );
     }
 }
