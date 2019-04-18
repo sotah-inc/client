9904d0a2d67c5479e91407707cce07e003662a97
Adrian Parker <ihsw.aparker@gmail.com>
2019-04-17 22:00:14 -0400

    Start profile api.

---
+++ app/src/api/profile.ts
@@ -0,0 +1,44 @@
+import * as HTTPStatus from "http-status";
+
+import { IValidationErrorResponse } from "@app/api-types/contracts";
+import { IUpdateProfileRequest, IUpdateProfileResponse } from "@app/api-types/contracts/user/profile";
+import { apiEndpoint, gather } from "./index";
+
+export interface IUpdateProfileResult {
+    email: string | null;
+    error?: string;
+    errors?: {
+        [key: string]: string;
+    };
+}
+
+export const updatePost = async (
+    token: string,
+    id: number,
+    request: IUpdateProfileRequest,
+): Promise<IUpdateProfileResult> => {
+    const { body, status } = await gather<IUpdateProfileRequest, IUpdateProfileResponse | IValidationErrorResponse>({
+        body: request,
+        headers: new Headers({
+            Authorization: `Bearer ${token}`,
+            "content-type": "application/json",
+        }),
+        method: "PUT",
+        url: `${apiEndpoint}/user/profile`,
+    });
+    if (status === HTTPStatus.UNAUTHORIZED) {
+        return { error: "Unauthorized", email: null };
+    }
+    switch (status) {
+        case HTTPStatus.OK:
+            break;
+        case HTTPStatus.UNAUTHORIZED:
+            return { error: "Unauthorized", email: null };
+        case HTTPStatus.BAD_REQUEST:
+            return { errors: body as IValidationErrorResponse, email: null };
+        default:
+            return { error: "Failure", email: null };
+    }
+
+    return { email: body!.email };
+};
