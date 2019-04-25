395f670b8edb0e28a9ffb8485b6dabf5b9fc6072
Adrian Parker <ihsw.aparker@gmail.com>
2019-04-24 21:17:50 -0400

    Start filling in prompts.

--- app/src/components/App/Prompts.tsx
+++ app/src/components/App/Prompts.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 
+import { Button, Callout, Intent } from "@blueprintjs/core";
 import { RouteComponentProps } from "react-router-dom";
 
-import { IUserJson } from "@app/api-types/entities";
+import { IUserJson, UserLevel } from "@app/api-types/entities";
 
 export interface IStateProps {
     user: IUserJson | null;
@@ -18,6 +19,36 @@ export type Props = Readonly<IStateProps & IDispatchProps & IOwnProps>;
 
 export class Prompts extends React.Component<Props> {
     public render() {
-        return <p>Hello, world</p>;
+        const { user } = this.props;
+
+        if (user === null) {
+            return null;
+        }
+
+        if (user.level === UserLevel.Unverified) {
+            return this.renderUnverified();
+        }
+
+        return null;
+    }
+
+    private renderUnverified() {
+        return (
+            <div style={{ marginBottom: "10px" }}>
+                <Callout intent={Intent.PRIMARY} title="Unverified Account">
+                    <p>Your account is not verified!</p>
+                    <p>
+                        <Button
+                            icon={"envelope"}
+                            intent={Intent.PRIMARY}
+                            onClick={() => {
+                                console.log("wew lad");
+                            }}
+                            text="Verify with Email"
+                        />
+                    </p>
+                </Callout>
+            </div>
+        );
     }
 }
